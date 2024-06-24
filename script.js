a9d212b86af9a47ddf7553c441941fdd44a63b9c
Larissa Cassanjo <larissacassanjo@gmail.com>
2024-06-24 19:07:30 -0300

    adicionando funcionalidades js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const ligar = document.getElementById("ligar");
+const desligar = document.getElementById("desligar");
+const lampada = document.getElementById("lampada");
 
+function estaQuebrada() {
+    return lampada.src.indexOf("quebrada") > -1;
+}
+
+function acender() {
+    if (!estaQuebrada()) {
+        lampada.src = "./img/acesa.png";
+    }
+}
+function apagar() {
+    if (!estaQuebrada()) {
+        lampada.src = "./img/desligada.png";
+    }
+}
+function quebrar() {
+    lampada.src = "./img/quebrada.png";
+}
+
+ligar.addEventListener("click", acender);
+desligar.addEventListener("click", apagar);
+lampada.addEventListener("dblclick", quebrar);
